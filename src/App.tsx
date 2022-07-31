f10a055fca5df181ddb1b51a22fa78bda944c234
inaridiy <inainainariinainari@gmail.com>
2022-08-01 00:57:09 +0900

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <div className="bg-base-200 flex h-screen w-full items-center justify-center px-4">
-      <div className="card card-body bg-base-100 shadow-lg">
-        <h1 className="text-xl font-bold">Welcome Web2 Template.</h1>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
