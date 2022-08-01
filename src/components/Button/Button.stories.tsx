a3cbe4a0008cd344aaf4c3ff8db4af67c7188d0b
inaridiy <inainainariinainari@gmail.com>
2022-08-01 12:56:08 +0900

    Implement button components

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,8 +1,29 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Button } from "./Button";
 
 export default {
-  title: "Button",
+  title: "Element/Button",
   component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>Hello World</Button>
+);
+
+export const normal = Template.bind({});
+normal.args = {
+  children: "Hello World",
+  color: "normal",
 };
 
-export const HelloButton = () => <Button>Hello World</Button>;
+export const primary = Template.bind({});
+primary.args = {
+  children: "Hello World",
+  color: "primary",
+};
+
+export const secondary = Template.bind({});
+secondary.args = {
+  children: "Hello World",
+  color: "secondary",
+};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,39 @@
-export const Button: React.FC<JSX.IntrinsicElements["button"]> = (props) => {
-  return <button {...props} className="btn"></button>;
+import clsx from "clsx";
+
+export type ButtonProps = {
+  children: React.ReactNode;
+  size: "sm" | "md" | "lg";
+  color: "normal" | "primary" | "secondary" | "info" | "warn" | "error";
+};
+const buttonSizes = {
+  sm: "px-2 py-1 text-sm",
+  md: "px-4 py-2 text-normal",
+  lg: "px-6 py-3 text-xl",
+};
+
+const buttonColors = {
+  normal: "bg-base-100 text-base-content",
+  primary: "bg-primary text-primary-content",
+  secondary: "bg-secondary text-secondary-content",
+  info: "bg-info text-info-content",
+  warn: "bg-warning text-warning-content",
+  error: "bg-error text-error-content",
+};
+
+export const Button: React.FC<Partial<ButtonProps>> = ({
+  children,
+  size = "md",
+  color = "normal",
+}) => {
+  return (
+    <button
+      className={clsx(
+        "border-base-content rounded-full border-2 font-bold",
+        buttonSizes[size],
+        buttonColors[color]
+      )}
+    >
+      {children}
+    </button>
+  );
 };
