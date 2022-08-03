8002c9de62f9ed884288b7534a6d7942eac68078
inaridiy <inainainariinainari@gmail.com>
2022-08-03 23:56:46 +0900

    pwa update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,39 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,txt,mp4}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "すーぱー転送君",
+        short_name: "転送君",
+        description: "ファイルとか送れるぜ",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": `${__dirname}/src/`,
     },
   },
-})
+});
